8a1b60bc8d77a288cb741c245ff4bb96fa5e78f1
richardangell <richardangell37@gmail.com>
2019-02-23 12:16:53 +0000

    re work bucketing functions

--- R/discretise.R
+++ R/discretise.R
@@ -1,72 +1,258 @@
 
-bin_ordered <- function(ordered, weights, bins = 30, binning = "equal_width") {
 
-  if (binning == "equal_width") {
 
-    ordered_binned <- cut(ordered, breaks = bins, include.lowest = T, ordered_result = T)
 
-    ordered_binned <- addNA(ordered_binned)
 
-    if (any(is.na(levels(ordered_binned)))) {
+discretise <- function(x, w = NULL, bins = 10, type = NULL, q = NULL, ...) {
 
-      levels(ordered_binned)[which(is.na(levels(ordered_binned)))] <- "NA"
+  if (!is.vector(x)) {
 
-    }
-
-  } else if (binning == "equal_weight") {
-
-    df <- data.frame(ordered = ordered,
-                     weights = weights)
-
-    total_weight <- sum(df$weights)
-
-    if (sum(is.na(df$ordered)) > 0) {
-
-      df <- df[!is.na(df$ordered), ]
-
-    }
-
-    total_weight_excl_NA <- sum(df$weight)
-
-    df <- df[order(df$ordered, na.last = TRUE, decreasing = FALSE), ]
-
-    df$cum_weight <- cumsum(df$weights)
-
-    bucket_weight <- total_weight / bins
-
-    buckets_cum_weight <- c(0,
-                            (1:(bins-1)) * bucket_weight,
-                            total_weight_excl_NA)
-
-    bucket_indices <- sapply(buckets_cum_weight,
-                             function(x) which(df$cum_weight >= x)[1])
-
-    if (any(bucket_indices == 0, na.rm = TRUE)) {
-
-      bucket_indices <- bucket_indices[-which(bucket_indices == 0)]
-
-    }
-
-    ordered_binned <- cut(ordered,
-                          breaks = unique(df$ordered[bucket_indices]),
-                          include.lowest = TRUE)
-
-    ordered_binned <- addNA(ordered_binned)
-
-    if (any(is.na(levels(ordered_binned)))) {
-
-      levels(ordered_binned)[which(is.na(levels(ordered_binned)))] <- "NA"
-
-    }
-
-  } else {
-
-    stop(gettextf("unexpected value for binning argument; (%s)",
-                  sQuote(binning)))
+    stop('x should be a vector')
 
   }
 
-  return(ordered_binned)
+  # can support ordered factors in the future
+  if (!is.numeric(x)) {
+
+    stop('x should be numeric')
+
+  }
+
+  n <- length(x)
+
+  if (is.null(w)) {
+
+    w <- rep(1, length(x))
+
+  } else {
+
+    if (length(w) != n) {
+
+      stop('w and x are different lengths')
+
+    }
+
+    if (any(is.null(w))) {
+
+      stop('w has null values')
+
+    }
+
+    if (any(w < 0)) {
+
+      stop('w have negative values')
+
+    }
+
+  }
+
+  if (!is.numeric(bins)) {
+
+    stop('bins must be numeric')
+
+  }
+
+  if (!bins > 1) {
+
+    stop('bins must be > 1')
+
+  }
+
+  type_accepted_values <-
+    c('equal_weight', 'equal_width', 'quantiles')
+
+  if (!type %in% type_accepted_values) {
+
+    stop('unexpected value for type')
+
+  }
+
+  if (type == 'quantiles' & is.null(q)) {
+
+    stop('quantiles have not been supplied')
+
+  }
+
+  if (!is.null(q)) {
+
+    if (!is.numeric(q)) {
+
+      stop('q should be numeric')
+
+    }
+
+    if (!all(q >= 0 & q <= 1)) {
+
+      stop('q values should be between 0 and 1')
+
+    }
+
+  }
+
+  if (type == 'equal_width') {
+
+    x_binned <- bin_equal_width(x = x, bins = bins, ...)
+
+  } else if (type == 'equal_weight') {
+
+    x_binned <- bin_equal_weight(x = x, w = w, bins = bins)
+
+  } else if (type == 'quantiles') {
+
+    x_binned <- bin_quantiles(x = x, q = q, ...)
+
+  } else {
+
+    stop('unexpected type')
+
+  }
+
+  return(x_binned)
 
 }
+
+
+
+
+bin_equal_width <- function(x, bins, ...) {
+
+  x_binned <- cut(x,
+                  breaks = bins,
+                  include.lowest = TRUE,
+                  ordered_result = TRUE,
+                  ...)
+
+  x_binned_na <- add_character_NA(x_binned)
+
+  return(x_binned_na)
+
+}
+
+
+
+
+bin_equal_weight <- function(x, w, bins) {
+
+  q <- seq(0, bins, 1) / bins
+
+  q_weighted <- weighted_percentile(x, w, q)
+
+  x_binned <- cut(x,
+                  breaks = q_weighted,
+                  include.lowest = TRUE,
+                  ordered_result = TRUE,
+                  ...)
+
+  x_binned_na <- add_character_NA(x_binned)
+
+  return(x_binned_na)
+
+}
+
+bin_min_weight <- function() {
+
+
+}
+
+
+
+bin_quantiles <- function(x, q, ...) {
+
+  quantiles <- quantile(x, probs = q)
+
+  x_binned <- cut(x,
+                  breaks = quantiles,
+                  include.lowest = TRUE,
+                  ordered_result = TRUE,
+                  ...)
+
+  x_binned_na <- add_character_NA(x_binned)
+
+  return(x_binned_na)
+
+}
+
+
+
+add_character_NA <- function(x) {
+
+  if (!is.factor(x)) {
+
+    stop('x should be a factor')
+
+  }
+
+  if (sum(is.na(x)) > 0) {
+
+    x_na <- addNA(x)
+
+    levels(x_na)[which(is.na(levels(x_na)))] <- "NA"
+
+    return(x_na)
+
+  } else {
+
+    return(x)
+
+  }
+
+}
+
+
+
+
+
+weighted_percentile <- function(x, w, q) {
+
+  n <- length(x)
+
+  x_order <- order(x)
+
+  w <- w[x_order]
+
+  x <- x[x_order]
+
+  w_pct <- cumsum(w) / sum(w)
+
+  w_q <- rep(0, length(q))
+
+  for (i in 1:length(q)) {
+
+    p <- q[i]
+
+    gt <- which(w_pct >= p)[1]
+
+    # if the quantile value has been found exactly return that value
+    if (w_pct[gt] == p) {
+
+      w_quantile <- x[gt]
+
+    # otherwise interpolate between
+    } else {
+
+      # if the first value in x meets the condition don't interpolate
+      if (gt == 1) {
+
+        w_quantile <- x[gt]
+
+      } else {
+
+        lt <- gt - 1
+
+        w_quantile <- x[lt] +
+          (x[gt] - x[lt]) * ((p - w_pct[lt]) / (w_pct[gt] - w_pct[lt]))
+
+      }
+
+    }
+
+    w_q[i] <- w_quantile
+
+  }
+
+  return(w_q)
+
+}
+
+
 
